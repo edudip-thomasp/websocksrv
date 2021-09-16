f915b783aee4197274a2c09ba025d49dcc47ad52
edudip-thomasp <t.puetz@edudip.com>
2021-09-16 11:59:41 +0200

    Implement registering and unregistering of clients

--- main.go
+++ main.go
@@ -29,16 +29,20 @@ func (clh *clientHub) run() {
 	for {
 		select {
 		case client := <-clh.register:
+			log.Printf("Registering client: %v\n", client)
 			clh.clients = append(clh.clients, client)
 		case client := <-clh.unregister:
 			for i, reggedClient := range clh.clients {
 				if client == reggedClient {
+					log.Printf("Client array BEFORE unregistering: %v\n", clh.clients)
 					clh.clients = append(clh.clients[:i], clh.clients[i+1:]...)
+					log.Printf("Client array AFTER unregistering: %v\n", clh.clients)
 					break
 				}
 			}
 		case cmm := <-clh.broadcast:
 			for _, reggedClient := range clh.clients {
+				log.Printf("Broadcastring to client: %v\n", reggedClient)
 				if cmm.client != reggedClient {
 					reggedClient.sendChan <- cmm.message
 				}
@@ -56,12 +60,19 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 	log.Printf("%v", w)
 }
 
-func reader(c *clientRepr, ch *clientHub) int {
+func reader(c *clientRepr, ch *clientHub) {
+	defer func() {
+		ch.unregister <- c
+	    c.conn.Close()
+	}()
+
 	for {
 		log.Printf("websocket reader(): At client: %v\n", c)
 		_, p, err := c.conn.ReadMessage()
+
 		if err != nil {
-			log.Fatal(err)
+			log.Println("Error in websocket reader() detected: Maybe socket connection closed by client.")
+			break
 		}
 
 		if len(p) > 0 {
@@ -77,14 +88,18 @@ func reader(c *clientRepr, ch *clientHub) int {
 }
 
 func writer(c *clientRepr, ch *clientHub) {
+	defer func() {c.conn.Close()}()
+
 	for {
 		log.Printf("websocket writer(): At client: %v\n", c)
 
 		select {
 		case broadcastMsg := <-c.sendChan:
-			err2 := c.conn.WriteMessage(websocket.TextMessage, broadcastMsg)
-			if err2 != nil {
-				log.Fatal(err2)
+			writeErr := c.conn.WriteMessage(websocket.TextMessage, broadcastMsg)
+			if writeErr != nil {
+				log.Printf("Something went wrong when writing message to client. Closing thread...\n")
+				log.Printf("Client: %+v\n error: %+v\n", c, writeErr)
+				break
 			}
 		}
 	}
@@ -108,7 +123,7 @@ func wsEndpoint(w http.ResponseWriter, r *http.Request) {
 		sendChan: make(chan []byte),
 	}
 
-	hub.clients = append(hub.clients, client)
+	hub.register <- client
 	log.Printf("wsEndpoint(): Client registered just now: %v\n", client)
 
 	go reader(client, &hub)
